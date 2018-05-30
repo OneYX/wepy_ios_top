e64564e24001f2eba16a2fd8a19d1105a686330d
OneYX <504887284@qq.com>
2018-05-30 14:06:19 +0800

    ''

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise'
+import logger from 'redux-logger'
 import rootReducer from './reducers'
 
 export default function configStore () {
-  const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
+  const store = createStore(rootReducer, applyMiddleware(promiseMiddleware, logger))
   return store
 }
